60b1e5d07ddd5999cb8dab34db40b50061eca1ea
qb3 <quentinbertrand54@gmail.com>
2017-12-20 17:54:08 +0100

    change the distanceSuperPatcheL2 functions arguemnts

--- distanceSuperPatchL2.m
+++ distanceSuperPatchL2.m
@@ -1,26 +1,24 @@
-function dist = distanceSuperPatchL2(superPatchA, superPatchB, image, idx)
+function dist = distanceSuperPatchL2(superPatchA, superPatchB, imageA, imagB, idxA, idxB)
+end
+
+
+function dist = distanceSuperPixelL2(superPatchA, superPatchB, imageA, imagB, idxA, idxB)
 %superPatchA is the list of the label of the superpixels in the superpatchA
     dist = 0;
     for color = 0:2
-        color
-        histA = getHist(superPatchA, image, idx,  color);  
-        histB = getHist(superPatchB, image, idx,  color) ;
+        histA = getHist(superPixelA, imageA, idxA,  color);  
+        histB = getHist(superPixelB, imageB, idxB,  color) ;
         diff2 = (histA - histB).^2;
         dist = dist + mean(diff2);
     end
 end
 
- function histo = getHist(superPatchA, im, idx,  n)
-    n
-    [numRows,numCols, plop]  = size(im);
-    numRows
-    numCols
-    
-    redIdxA = idx{superPatchA};
-    redIdxA + n*numRows*numCols;
-    redValues = im(redIdxA);
-    imhistoA = hist(redValues,[0:255]);  % or imhist(imgray(:),256); with Scilab 
-    imhistoA =imhistoA/sum(imhistoA);
-    histo = cumsum(imhistoA); 
+ function histo = getHist(superPixel, im, idx,  n)
+    [numRows,numCols, dim]  = size(im);
+    Idx = idx{superPixel};
+    Idx = Idx + n*numRows*numCols;
+    Values = im(Idx);
+    imhisto = hist(Values,[0:255]);  % or imhist(imgray(:),256); with Scilab 
+    imhisto =imhisto/sum(imhisto);
+    histo = cumsum(imhisto); 
  end
- 
