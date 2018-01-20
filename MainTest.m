fe62f6c13895567278e1b8303592d9ff1f029182
mathbarre <mathieu.barre83@gmail.com>
2018-01-20 17:16:31 +0100

    add pictures

--- MainTest.m
+++ MainTest.m
@@ -1,36 +1,36 @@
-%im = (imread('TP/im/scotland_house.png')); 
-%pathImA = 'TP/im/flower3.jpeg';
-%pathImB = 'TP/im/flower2.bmp';
-%pathImA = 'TP/im/forest_summer.jpg';
-%pathImB = 'TP/im/forest_autumn.jpg';
-%pathImB = 'TP/im/dicaprio.jpg';
-%pathImA = 'TP/im/avatar.jpg';
-%pathImA = 'TP/im/ice_dark.jpg';
-%pathImB = 'TP/im/ice_clear.jpg';
-%pathImB = 'TP/im/sun_ice.jpg';
-%pathImA = 'TP/im/sunset_ice.jpg';
-%pathImB = 'TP/im/flower1.bmp';
-%pathImA = 'TP/im/flower2.bmp';
-%pathImA = 'TP/im/ice_dark.jpg';
-%pathImB = 'TP/im/sunset2.jpg';
-%pathImA = 'TP/im/avatar.jpg';
-%pathImB = 'TP/im/femme.jpg';
-%pathImA = 'TP/im/adachi_moins_vert.jpeg';
-%pathImB = 'TP/im/adachi_vert.jpg';
-pathImA = 'TP/im/scotland_house.png';
-pathImB = 'TP/im/scotland_plain.png';
-%pathImB ='TP/im/scotland_house.png';
-%pathImA = 'TP/im/scotland_house.png';
-%pathImB = 'TP/im/tropique.jpg'
+%im = (imread('im/scotland_house.png')); 
+%pathImA = 'im/flower3.jpeg';
+%pathImB = 'im/flower2.bmp';
+%pathImA = 'im/forest_summer.jpg';
+%pathImB = 'im/forest_autumn.jpg';
+%pathImB = 'im/dicaprio.jpg';
+%pathImA = 'im/avatar.jpg';
+%pathImA = 'im/ice_dark.jpg';
+%pathImB = 'im/ice_clear.jpg';
+%pathImB = 'im/sun_ice.jpg';
+%pathImA = 'im/sunset_ice.jpg';
+%pathImB = 'im/flower1.bmp';
+%pathImA = 'im/flower2.bmp';
+%pathImA = 'im/ice_dark.jpg';
+%pathImB = 'im/sunset2.jpg';
+%pathImA = 'im/avatar.jpg';
+%pathImB = 'im/femme.jpg';
+%pathImA = 'im/adachi_moins_vert.jpeg';
+%pathImB = 'im/adachi_vert.jpg';
+pathImA = 'im/scotland_house.png';
+pathImB = 'im/scotland_plain.png';
+%pathImB ='im/scotland_house.png';
+%pathImA = 'im/scotland_house.png';
+%pathImB = 'im/tropique.jpg'
 
-%pathImA = 'TP/im/fuji_automne.jpg';
-%pathImB = 'TP/im/fuji_hiver.jpg';
+%pathImA = 'im/fuji_automne.jpg';
+%pathImB = 'im/fuji_hiver.jpg';
 
 %pathImA = 'Results/scotland_plain_on_scotland_house_eps_3_lambda_0.001_R_60_regrain.png';
 nameA = strsplit(pathImA,{'/','.'});
-nameA = nameA{3};
+nameA = nameA{2};
 nameB = strsplit(pathImB,{'/','.'});
-nameB = nameB{3};
+nameB = nameB{2};
 im = (imread(pathImA));
 imB = (imread(pathImB)); 
 
@@ -51,4 +51,7 @@ resSCT = SCT(im,imB,epsilon,lambda,R,0.7,15,nbSuperPixelsWantedA,nbSuperPixelsWa
 figure;imshow(resSCT,[]);
 Final = regrain(double(im)/255,resSCT);
 figure;imshow(Final,[]);
+
+%imwrite(TransformedImage/255, strcat('Results/',nameB,'_on_',nameA,'_eps_',num2str(epsilon),'_lambda_',num2str(lambda),'_R_',num2str(R),'.png'));
+%imwrite(Final, strcat('Results/',nameB,'_on_',nameA,'_eps_',num2str(epsilon),'_lambda_',num2str(lambda),'_R_',num2str(R),'_regrain.png'));
 
